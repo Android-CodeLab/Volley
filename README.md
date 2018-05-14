86a198900fabf06ec405b949c296b092a578b388
LibJamun <jatinsahgal50@gmail.com>
2018-05-15 01:14:27 +0530

    0.0.1-beta (v1)

---
+++ README.md
@@ -0,0 +1,14 @@
+<h1><b>Jamun-Volley</b></h1>
+<h3>A set of Custom Classes with UI components for network programming, integration and transaction handling in a better and standard way. This will help developers for making quality use of volley library.</h3>
+<br>
+<h1><b>Hi Developers</b></h1>
+<b>Here,</b><br>
+  <b>1.</b> In this Library Jamun-Volley, we provide you a package of custom Volley Classes which ease your work while working with API calls.
+  <b>2.</b> You just need to setup all the configuration stuff (Like Header Meta-Data, Singleton Declarations this will be shown with example later in the documnet) at one place so that you don't have to write same piece of code again and again. For example, MyApplication Class.<br>
+  <b>3.</b> Jamun-Volley library has been developed keeping many parameters in mind by default the library support many auto-configuration like LUR-Cache, Request and ImageLoader Objects. But in case if you need To Edit Something We have also provided you to edit Definations and setting up new configuration parameters.<br>
+  <b>4.</b> One of the main benifit of using Jamun-Volley LIbrary is that you can Post Any type of data and Retrive response in another format which wasn't possible in traditional Volley Library.<br>
+  <b>5.</b> THis Library also provide you File Downloading and Uploading classes which were upsent in Traditional Volley. With the help of this Library you can also manage of your file Downloading/Uploading progress easliy with Data Statistics.<br>
+  <b>6.</b> Library's most attractives UI Feature is that you are provided with CircularNetworkImageView with the help of which you can display your image in Attractive Circullar View. By Just using CircularNetworkImageView.<br>
+  <b>7.</b> Library also provide you a UI Component to Show progress Data Statistics in Notification. But this also provide you a set of Function which help in Customization.<br>
+  
+  
---
+++ sample/src/main/java/in/jamun/MainActivity.java
@@ -0,0 +1,318 @@
+package in.jamun;
+
+import android.os.Bundle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+
+import in.jamun.pickers.extras.PickerListenerCountry;
+import in.jamun.pickers.extras.PickerListenerLanguage;
+import in.jamun.pickers.models.ModelCountryDetails;
+import in.jamun.pickers.models.ModelDialog;
+import in.jamun.pickers.models.ModelIntentPicker;
+import in.jamun.pickers.models.ModelLanguage;
+import in.jamun.pickers.models.ModelLanguageParent;
+import in.jamun.pickers.views.country.PickerCountryActivity;
+import in.jamun.pickers.views.country.PickerCountryBottom;
+import in.jamun.pickers.views.country.PickerCountryDialog;
+import in.jamun.pickers.views.country.PickerCountryNavigation;
+import in.jamun.pickers.views.intents.PickerIntent;
+import in.jamun.pickers.views.intents.PickerShareFiles;
+import in.jamun.pickers.views.language.PickerLanguageActivity;
+import in.jamun.pickers.views.language.PickerLanguageBottom;
+import in.jamun.pickers.views.language.PickerLanguageDialog;
+import in.jamun.pickers.views.language.PickerLanguageNavigation;
+import in.jamun.sample.R;
+
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_ARABIC;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_ASSAMESE;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_BENGALI;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_ENGLISH;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_GUJRATI;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_HINDI;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_KANNADA;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_MAITHALI;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_MALAYALAM;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_MARATHI;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_MODE_NORMAL;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_ODIA;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_PUNJABI;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_TAMIL;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_TELGU;
+import static in.jamun.pickers.extras.InterfaceLanguageListener.LANG_URDU;
+
+public class MainActivity extends AppCompatActivity {
+    private PickerCountryBottom pickerCountryBottom;
+    private PickerLanguageBottom pickerLanguageBottom;
+    private static final int ACTION_REQUEST_CAMERA = 111;
+    private PickerIntent intentPicker;
+    private PickerShareFiles pickerShareFiles;
+    private DrawerLayout drawerLayout;
+    private Toolbar toolbar;
+    private PickerCountryDialog pickerCountryDialog;
+    private PickerLanguageDialog pickerLanguageDialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setToolbar();
+        initializeShareIntent();
+        initializeNavigationViewLanguage();
+        initializeNavigationViewCountry();
+        initializeCountryDialog();
+        initializeLanguageDialog();
+        initializeCountryBottom();
+        initializeLanguageBottom();
+    }
+
+    private void initializeShareIntent() {
+        pickerShareFiles = new PickerShareFiles();
+        pickerShareFiles.setThings(this);
+    }
+
+    private void initializeLanguageDialog() {
+        pickerLanguageDialog = new PickerLanguageDialog(this).setData(initializeData()).
+                bindListener(new PickerListenerLanguage() {
+                    @Override
+                    public void singleModeData(ModelLanguage modelLanguage) {
+                        super.singleModeData(modelLanguage);
+                    }
+
+
+                    @Override
+                    public void multiModeData(ArrayList<ModelLanguage> languageArrayList) {
+                        super.multiModeData(languageArrayList);
+                    }
+                }).setSelectionModeMulti(false).createSetUp();
+    }
+
+    private void initializeLanguageBottom() {
+        pickerLanguageBottom = new PickerLanguageBottom().setThings(this, initializeData())
+                .bindListener(new PickerListenerLanguage() {
+                    @Override
+                    public void singleModeData(ModelLanguage modelLanguage) {
+                        super.singleModeData(modelLanguage);
+                    }
+
+                    @Override
+                    public void multiModeData(ArrayList<ModelLanguage> languageArrayList) {
+                        super.multiModeData(languageArrayList);
+                    }
+                });
+        pickerLanguageBottom.setSelectionModeMulti(false);
+    }
+
+    private void initializeCountryBottom() {
+        pickerCountryBottom = new PickerCountryBottom().setThings(this)
+                .bindListener(new PickerListenerCountry() {
+                    @Override
+                    public void singleModeData(ModelDialog modelLanguage) {
+                        super.singleModeData(modelLanguage);
+                    }
+
+                    @Override
+                    public void multiModeData(ArrayList<ModelDialog> languageArrayList) {
+                        super.multiModeData(languageArrayList);
+                    }
+
+                    @Override
+                    public void apiError(int statusCode, String errorMessage) {
+                    }
+                });
+        pickerCountryBottom.setSelectionModeMulti(false);
+    }
+
+    private void initializeCountryDialog() {
+        pickerCountryDialog = new PickerCountryDialog(this).bindListener(new PickerListenerCountry() {
+            @Override
+            public void singleModeData(ModelDialog modelLanguage) {
+                super.singleModeData(modelLanguage);
+            }
+
+            @Override
+            public void multiModeData(ArrayList<ModelDialog> languageArrayList) {
+                super.multiModeData(languageArrayList);
+            }
+
+            @Override
+            public void apiError(int statusCode, String errorMessage) {
+
+            }
+        }).setSelectionModeMulti(false).setTags(initializeDataCountryTags()).createSetUp();
+    }
+
+    private void setToolbar() {
+        toolbar = findViewById(R.id.id_app_bar);
+        setSupportActionBar(toolbar);
+        drawerLayout = findViewById(R.id.id_drawer);
+        drawerLayout.setDescendantFocusability(ViewGroup.FOCUS_AFTER_DESCENDANTS);
+    }
+
+    private void initializeNavigationViewLanguage() {
+        PickerLanguageNavigation navLeftFragment = (PickerLanguageNavigation)
+                getSupportFragmentManager().findFragmentById(R.id.id_navigation_language);
+        navLeftFragment.setSelectionModeMulti(false);
+        navLeftFragment.setUpDrawer(drawerLayout, toolbar, false, new PickerListenerLanguage() {
+            @Override
+            public void singleModeData(ModelLanguage modelLanguage) {
+                super.singleModeData(modelLanguage);
+            }
+
+            @Override
+            public void multiModeData(ArrayList<ModelLanguage> languageArrayList) {
+                super.multiModeData(languageArrayList);
+            }
+        }, LANG_MODE_NORMAL, GravityCompat.START);
+    }
+
+    private void initializeNavigationViewCountry() {
+        PickerCountryNavigation navLeftFragment = (PickerCountryNavigation)
+                getSupportFragmentManager().findFragmentById(R.id.id_navigation);
+        navLeftFragment.setSelectionModeMulti(false);
+        navLeftFragment.setTags(initializeDataCountryTags());
+        navLeftFragment.setUpDrawer(drawerLayout, toolbar, false, new PickerListenerCountry() {
+            @Override
+            public void singleModeData(ModelDialog modelLanguage) {
+                super.singleModeData(modelLanguage);
+            }
+
+            @Override
+            public void multiModeData(ArrayList<ModelDialog> languageArrayList) {
+                super.multiModeData(languageArrayList);
+            }
+
+            @Override
+            public void apiError(int statusCode, String errorMessage) {
+            }
+        }, GravityCompat.END);
+    }
+
+    public void onClickIntent(View view) {
+        if (intentPicker == null) {
+            ArrayList<ModelIntentPicker> arrayList = new ArrayList<>();
+            arrayList.add(new ModelIntentPicker(PickerIntent.PICKER_CAMERA, getString(R.string.string_button_name_camera), R.drawable.library_icon_vd_clear,
+                    R.drawable.library_background_intent_two, ACTION_REQUEST_CAMERA));
+            arrayList.add(new ModelIntentPicker(PickerIntent.PICKER_REMOVE, getString(R.string.string_button_name_remove), R.drawable.library_icon_vd_clear, R.drawable.
+                    library_background_intent_five, 0));
+            intentPicker = new PickerIntent();
+            intentPicker.setThings(this)
+                    .setPicker("Choose Intent Picker", arrayList);
+        }
+        intentPicker.showPicker(getSupportFragmentManager());
+    }
+
+    public void onClickShare(View view) {
+        pickerShareFiles.shareThings(getSupportFragmentManager(), "Description", "Title");
+    }
+
+    public void onClickCountry(View view) {
+        pickerCountryBottom.showPicker(getSupportFragmentManager());
+    }
+
+    public void onClickCountryNavigation(View view) {
+//        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+        drawerLayout.openDrawer(GravityCompat.END);
+    }
+
+    public void onClickCountryDialog(View view) {
+        pickerCountryDialog.show();
+    }
+
+    public void onClickCountryActivity(View view) {
+        initializeDataCountryTags();
+        PickerCountryActivity.setThings(this, new PickerListenerCountry() {
+            @Override
+            public void singleModeData(ModelDialog modelLanguage) {
+                super.singleModeData(modelLanguage);
+            }
+
+            @Override
+            public void multiModeData(ArrayList<ModelDialog> languageArrayList) {
+                super.multiModeData(languageArrayList);
+            }
+
+            @Override
+            public void apiError(int statusCode, String errorMessage) {
+
+            }
+        }, initializeDataCountryTags(), false);
+    }
+
+    private ModelCountryDetails initializeDataCountryTags() {
+        ModelCountryDetails modelCountryDetails = new ModelCountryDetails();
+        modelCountryDetails.setTagCountry("country");
+        modelCountryDetails.setTagISO("iso");
+        modelCountryDetails.setTagCountryCode("countrycode");
+        modelCountryDetails.setTagFlag("flag");
+        modelCountryDetails.setUrlImageDownload("http://flag.techcruzers.com/CountryFlag/");
+        modelCountryDetails.setUrlGetCountry("http://api.simplyblood.com/public/country/2");
+        return modelCountryDetails;
+    }
+
+    public void onClickLanguage(View view) {
+        pickerLanguageBottom.showPicker(getSupportFragmentManager());
+    }
+
+    public void onClickLanguageNavigation(View view) {
+//        drawerLayout.setDrawerLockMode(DrawerLayout.LOCK_MODE_UNLOCKED);
+        drawerLayout.openDrawer(GravityCompat.START);
+    }
+
+    public void onClickLanguageDialog(View view) {
+        pickerLanguageDialog.show();
+    }
+
+    public void onClickLanguageActivity(View view) {
+        PickerLanguageActivity.setThings(this, new PickerListenerLanguage() {
+            @Override
+            public void singleModeData(ModelLanguage modelLanguage) {
+                super.singleModeData(modelLanguage);
+            }
+
+            @Override
+            public void multiModeData(ArrayList<ModelLanguage> languageArrayList) {
+                super.multiModeData(languageArrayList);
+            }
+        }, initializeData(), LANG_MODE_NORMAL, false);
+    }
+
+    public ArrayList<ModelLanguageParent> initializeData() {
+        ArrayList<ModelLanguageParent> arrayList = new ArrayList<>();
+        ArrayList<ModelLanguage> arrayListChild = new ArrayList<>();
+        arrayListChild.add(new ModelLanguage(LANG_ENGLISH, "English", "English"));
+        arrayListChild.add(new ModelLanguage(LANG_HINDI, "Hindi", "हिंदी"));
+        arrayListChild.add(new ModelLanguage(LANG_ARABIC, "Arabic", "عربى"));
+        arrayList.add(new ModelLanguageParent(arrayListChild, getString(R.string.jamun_pickers_string_text_setting_select_language)));
+        arrayListChild = new ArrayList<>();
+        arrayListChild.add(new ModelLanguage(LANG_PUNJABI, "Punjabi", "ਪੰਜਾਬੀ"));
+        arrayListChild.add(new ModelLanguage(LANG_BENGALI, "Bengali", "বাঙালি"));
+        arrayListChild.add(new ModelLanguage(LANG_TELGU, "Telugu", "తెలుగు"));
+        arrayListChild.add(new ModelLanguage(LANG_MARATHI, "Marathi", "मराठी"));
+        arrayListChild.add(new ModelLanguage(LANG_TAMIL, "Tamil", "தமிழ்"));
+        arrayListChild.add(new ModelLanguage(LANG_URDU, "Urdu", "اردو"));
+        arrayListChild.add(new ModelLanguage(LANG_GUJRATI, "Gujarati", "ગુજરાતી"));
+        arrayListChild.add(new ModelLanguage(LANG_KANNADA, "Kannada", "ಕನ್ನಡ"));
+        arrayListChild.add(new ModelLanguage(LANG_ODIA, "Odia", "ଓରିୟା"));
+        arrayListChild.add(new ModelLanguage(LANG_MALAYALAM, "Malayalam", "മലയാളം"));
+        arrayListChild.add(new ModelLanguage(LANG_ASSAMESE, "Assamese", "অসমীয়া"));
+        arrayListChild.add(new ModelLanguage(LANG_MAITHALI, "Maithili", "মৈথিলী"));
+        arrayList.add(new ModelLanguageParent(arrayListChild, "Indian"));
+        return arrayList;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.END)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.END);
+        } else
+            super.onBackPressed();
+    }
+}
